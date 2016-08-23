9759e73a43195548b3fa3d37548a68a82de282d0
ssr42 <ssr@axiell.com>
2016-08-23 16:42:40 +0200

    Minor updates.

--- start-rfid.cmd
+++ start-rfid.cmd
@@ -1,5 +1,5 @@
 @echo off
-start "" "C:\Bibliotheca RFID\RFIDIFServer\RFIDIFServer.exe"
-start "" "C:\Bibliotheca RFID\RFIDIF\RFIDIF.exe"
+start "" "C:\Bibliotheca-RFID\RFIDIFServer\RFIDIFServer.exe"
+start "" "C:\Bibliotheca-RFID\RFIDIF\RFIDIF.exe"
 timeout 2 > NUL
 node server
